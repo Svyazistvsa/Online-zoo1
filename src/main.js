3acdee4e932daf3d46dc20351970eed5bba03d10
Svyazist <svyazist@desctop.Svyazist>
2026-01-15 22:37:52 +0500

    all header logos

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import 'normalize.css'
 import './style.css'
 import headerPanda from './header-panda.jpg'
+import logoText from './logoText.png'
+import bambuk from './bambuk.png'
 
 document.getElementsByTagName('header')[0].style.backgroundImage = `url("${headerPanda}")`
-
+document.getElementById('logoText').src = logoText;
+document.getElementsByClassName('bambuk')[0].src = bambuk;
 
 
 
