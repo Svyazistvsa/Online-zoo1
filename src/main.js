9f2fba02139c9faece39e85b17e5004257b94b3b
Svyazist <svyazist@desctop.Svyazist>
2026-01-16 17:00:34 +0500

    coperite and h1 height

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import './style.css'
 import headerPanda from './header-panda.jpg'
 import logoText from './logoText.png'
 import bambuk from './bambuk.png'
+import coperite from './coperite.png'
 
 document.getElementsByTagName('header')[0].style.backgroundImage = `url("${headerPanda}")`
 document.getElementById('logoText').src = logoText;
 document.getElementsByClassName('bambuk')[0].src = bambuk;
-
+document.querySelector('menu > img').src = coperite;
 
 
 
