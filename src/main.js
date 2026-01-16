7b9b3ef0322e71ecf8e79c7333d46a99888df24c
Svyazist <svyazist@desctop.Svyazist>
2026-01-16 18:31:39 +0500

    new main grid block - logoBox

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import logoText from './logoText.png'
 import bambuk from './bambuk.png'
 import coperite from './coperite.png'
 
-document.getElementsByTagName('header')[0].style.backgroundImage = `url("${headerPanda}")`
+document.getElementById('logoBox').style.backgroundImage = `url("${headerPanda}")`
 document.getElementById('logoText').src = logoText;
 document.getElementsByClassName('bambuk')[0].src = bambuk;
-document.querySelector('menu > img').src = coperite;
+document.querySelector('header > img').src = coperite;
 
 
 
