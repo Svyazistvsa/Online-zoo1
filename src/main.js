0f89fbf41d3d6ac77ddafebcaa0fb66b2e969fa9
Svyazist <svyazist@desctop.Svyazist>
2026-01-23 20:52:56 +0500

    start Pets

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import bambuk from './bambuk.png'
 import coperite from './coperite.png'
 import ded from './ded.png'
 import leafs1 from './leafs1.png'
+import flower from './flower.png'
+import bush from './bush.png'
 
 document.getElementById('logoBox').style.backgroundImage = `url("${headerPanda}")`
 document.getElementById('logoText').src = logoText;
@@ -13,4 +15,6 @@ document.getElementsByClassName('bambuk')[0].src = bambuk;
 document.querySelector('.coperite').style.backgroundImage = `url("${coperite}")`;
 document.getElementById('ded').style.backgroundImage = `url("${ded}")`;
 document.querySelector('.one').style.backgroundImage = `url("${leafs1}")`;
+document.querySelector('.two').style.backgroundImage = `url("${flower}")`;
+document.querySelector('.decorTwo').src = bush;
 
