830f10c4c3e4573302f85500a74ffa7e4460084a
Svyazist <svyazist@desctop.Svyazist>
2026-03-02 18:32:19 +0500

    cards.js is middle work

--- src/cards.js
+++ src/cards.js
@@ -1,7 +1,7 @@
 let cardsContainer = document.getElementById('cards');
 
 class Card {
-    constractor({name, picture, native}){
+    constructor({name, picture, native}){
         this.name = name;
         this.picture = picture;
         this.native = native;
@@ -10,34 +10,31 @@ class Card {
     buildNode(){
         let figure, img, figcaption;
         figure = document.createElement('figure');
-        img = document.createElement('img').src = this.picture;
-        figcaption = document.createElement('figcaption').innerHTML = `<span>${this.name}<span><br>${this.native}`;
+        img = document.createElement('img');
+        img.src = this.picture;
+        figcaption = document.createElement('figcaption');
+        figcaption.innerHTML = `<span>${this.name}</span><br>${this.native}`;
         
         figure.append(img);
         figure.append(figcaption);
         figure.classList.add('card');
         return figure;
     }
-
-    see(){
-        alert(this.name);
-    }
 }
 
 let cards = [
-    {name: 'giant Pandas', picture: 'url(./giant_pandas.png)', native: 'Native to Southwest China'},
-    {name: 'Gorillas', picture: 'url(./gorillas.png)', native: 'Native to Congo'},
-    {name: 'Eagles', picture: 'url(./eagles.png)', native: 'Native to South America'},
-    {name: 'Alligators', picture: 'url(./alligators.png)', native: 'Native to Southeastern U. S.'},
-    {name: 'cheetahs', picture: 'url(./cheetahs.png)', native: 'Native to Africa'},
-    {name: 'Penguins', picture: 'url(./penguins.png)', native: 'Native to Antarctica'},
-    {name: 'Two-toed Sloth', picture: 'url(./two-toed_sloth.png)', native: 'Mesoamerica, South America'},
+    {name: 'giant Pandas', picture: './src/giant_pandas.png', native: 'Native to Southwest China'},
+    {name: 'Gorillas', picture: './src/gorillas.png', native: 'Native to Congo'},
+    {name: 'Eagles', picture: './src/eagles.png', native: 'Native to South America'},
+    {name: 'Alligators', picture: './src/alligators.png', native: 'Native to Southeastern U. S.'},
+    {name: 'cheetahs', picture: './src/cheetahs.png', native: 'Native to Africa'},
+    {name: 'Penguins', picture: './src/penguins.png', native: 'Native to Antarctica'},
+    {name: 'Two-toed Sloth', picture: './src/two-toed_sloth.png', native: 'Mesoamerica, South America'},
 ];
 
 cards.forEach((item) => {
     let card = new Card(item);
-    card.see();
     let figure = card.buildNode();
     cardsContainer.append(figure);
-}, );
+});
 
