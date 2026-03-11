ef14a015746a8cc175f6f0a3d0076a1a9d674893
Svyazist <svyazist@desctop.Svyazist>
2026-03-11 19:29:44 +0500

    arrows is work

--- src/arrows.js
+++ src/arrows.js
@@ -1,26 +1,45 @@
-let leftButton = document.querySelector(".left"),
+document.addEventListener('DOMContentLoaded', function() {
+  let leftButton = document.querySelector(".left"),
     rightButton = document.querySelector(".right"),
     screen = document.querySelector(".twoOneCenter"),
     cardsField = screen.querySelector("#cards"),        
-    currentPosition = 0;   
+    currentPosition = 0,
+    cardArr, shift, gap, cardsFieldWidth, columnCount;
 
-    leftButton.addEventListener("click", fieldShift);
-    rightButton.addEventListener("click", fieldShift);
+  updateDimensions();
 
-    function fieldShift(e) {
-      let cards = cardsField.querySelectorAll('.card'),
-          cardWidth = cards[0].offsetWidth,  
-          gap = parseInt(getComputedStyle(cardsField).columnGap, 10),        
-          shift = cardWidth + gap,
-          columnCount = parseInt(document.documentElement.style.getPropertyValue("--cards_columns"), 10),          
-          cardsFieldWidth = cardWidth * columnCount + gap * (columnCount - 1),
-          max = screen.offsetWidth - cardsFieldWidth;
-      
-      if(e.currentTarget === leftButton && currentPosition != 0){
-        currentPosition += shift;        
-      }
-      if(e.currentTarget === rightButton && currentPosition != max && !!(1 - Math.sign(max))){
-        currentPosition -= shift;
-      }
-      cardsField.style.transform = `translateX(${currentPosition}px)`;
+  leftButton.addEventListener("click", fieldShift);
+  rightButton.addEventListener("click", fieldShift);
+
+  function fieldShift(e) {
+  
+    let max = screen.offsetWidth - cardsFieldWidth;
+  
+    if(e.currentTarget === leftButton && currentPosition != 0){
+      currentPosition += shift;        
     }
+    if(e.currentTarget === rightButton && currentPosition != max && !!(1 - Math.sign(max))){
+      currentPosition -= shift;
+    }
+    cardsField.style.transform = `translateX(${currentPosition}px)`;
+  }
+
+  function updateDimensions() {
+
+    if (!leftButton || !rightButton || !cardsField || cards.length === 0) {
+      console.error('Один из элементов не найден');
+      return;
+    }
+
+    cardArr = cardsField.querySelectorAll('.card');
+    cardWidth = cardArr[0].offsetWidth;  
+    gap = parseInt(getComputedStyle(cardsField).columnGap, 10);        
+    shift = cardWidth + gap;
+    columnCount = parseInt(document.documentElement.style.getPropertyValue("--cards_columns"), 10);
+    cardsFieldWidth = cardWidth * columnCount + gap * (columnCount - 1);
+  }
+
+  window.addEventListener('resize', () => {
+    updateDimensions();
+  });
+});
