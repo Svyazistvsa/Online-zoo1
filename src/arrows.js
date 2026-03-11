6cf36aa9c238a58f83f67909f3d4b526bee36b76
Svyazist <svyazist@desctop.Svyazist>
2026-03-11 18:51:48 +0500

    arrows is work

--- src/arrows.js
+++ src/arrows.js
@@ -1,23 +1,25 @@
 let leftButton = document.querySelector(".left"),
     rightButton = document.querySelector(".right"),
     screen = document.querySelector(".twoOneCenter"),
-    cardsField = screen.querySelector("#cards"),
-    card = cardsField.querySelector(".card"),
-    currentPosition = 0,
-    cardWidth = card.offsetWidth,
-    gap = getComputedStyle(cardsField).columnGap;
+    cardsField = screen.querySelector("#cards"),        
+    currentPosition = 0;   
 
     leftButton.addEventListener("click", fieldShift);
     rightButton.addEventListener("click", fieldShift);
 
     function fieldShift(e) {
-      let shift = +cardWidth + +gap.slice(0,-2),
-          max = cardsField.offsetWidth - screen.offsetWidth;
+      let cards = cardsField.querySelectorAll('.card'),
+          cardWidth = cards[0].offsetWidth,  
+          gap = parseInt(getComputedStyle(cardsField).columnGap, 10),        
+          shift = cardWidth + gap,
+          columnCount = parseInt(document.documentElement.style.getPropertyValue("--cards_columns"), 10),          
+          cardsFieldWidth = cardWidth * columnCount + gap * (columnCount - 1),
+          max = screen.offsetWidth - cardsFieldWidth;
       
-      if(e.currentTarget === leftButton, currentPosition != 0){
+      if(e.currentTarget === leftButton && currentPosition != 0){
         currentPosition += shift;        
       }
-      if(e.currentTarget === rightButton, currentPosition != max){
+      if(e.currentTarget === rightButton && currentPosition != max && !!(1 - Math.sign(max))){
         currentPosition -= shift;
       }
       cardsField.style.transform = `translateX(${currentPosition}px)`;
