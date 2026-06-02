65651bfe40715a7127a0cdf782092e5282228b9b
Svyazistvsa <sviazist.vsa@gmail.com>
2026-06-02 16:04:07 +0500

    get elements form

--- src/subscribe.js
+++ src/subscribe.js
@@ -1,2 +1,16 @@
 "use strict"
 
+let subscribeForm = document.forms.subscribe,
+    emailInput = subscribeForm.elements.email,
+    submit = subscribeForm.elements.submit;
+    
+function correct(){
+    let value = this.value;
+    if(value.includes("@")){
+        
+    }
+}
+
+emailInput.addEventListener("change",correct);
+
+
