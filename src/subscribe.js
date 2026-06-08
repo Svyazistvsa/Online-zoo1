ef1724d9d21fa8ffb377e62b1d5db9bbdd91fd62
Svyazistvsa <sviazist.vsa@gmail.com>
2026-06-08 16:28:12 +0500

    subscribe.js validate is vork

--- src/subscribe.js
+++ src/subscribe.js
@@ -3,14 +3,15 @@
 let subscribeForm = document.forms.subscribe,
     emailInput = subscribeForm.elements.email,
     submit = subscribeForm.elements.submit;
-    
-function correct(){
-    let value = this.value;
-    if(value.includes("@")){
-        
-    }
-}
 
-emailInput.addEventListener("change",correct);
+emailInput.addEventListener("change", function (e) {
+    const value = e.target.value;
+    const isValidDomain = value.includes("@")
+    if(!isValidDomain){
+        emailInput.style.color = "#D31414";
+    } else {
+        emailInput.style.color = "#fff";
+    }
+});
 
 
