3f287c9413424b71123dff40d44a7cd97717a2ce
Svyazistvsa <sviazist.vsa@gmail.com>
2026-06-17 15:13:55 +0500

    buttons in place

--- map/map.js
+++ map/map.js
@@ -2,5 +2,9 @@ import 'normalize.css'
 import './style.css'
 import bambuk from '../src/bambuk.png'
 import map from './map.png'
+import plus from './plus.png'
+import minus from './minus.png'
 
 document.querySelector(".map").style.backgroundImage = `url("${map}")`;
+document.querySelector(".plus").style.backgroundImage = `url("${plus}")`
+document.querySelector(".minus").style.backgroundImage = `url("${minus}")`
