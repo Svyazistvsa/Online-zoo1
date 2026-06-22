36d463cb3e5db4ce2663a3c01a4d7abaf3777170
Svyazistvsa <sviazist.vsa@gmail.com>
2026-06-22 15:48:10 +0500

    coperite is not hidden

--- map/map.js
+++ map/map.js
@@ -1,6 +1,7 @@
 import 'normalize.css'
 import './style.css'
 import bambuk from '../src/bambuk.png'
+import coperite from '../src/coperite.png'
 import map from './map.png'
 import plus from './plus.png'
 import minus from './minus.png'
@@ -8,3 +9,6 @@ import minus from './minus.png'
 document.querySelector(".map").style.backgroundImage = `url("${map}")`;
 document.querySelector(".plus").style.backgroundImage = `url("${plus}")`
 document.querySelector(".minus").style.backgroundImage = `url("${minus}")`
+document.getElementsByClassName('bambuk')[0].src = bambuk;
+//document.getElementsByClassName('bambuk')[1].src = bambuk;
+document.querySelector('.coperite').style.backgroundImage = `url("${coperite}")`;
