73c0a7467d857d1f8fcb46c909290ad6997bb396
Svyazistvsa <sviazist.vsa@gmail.com>
2026-06-26 15:26:10 +0500

    add points on map

--- map/map.js
+++ map/map.js
@@ -5,14 +5,18 @@ import coperite from '../src/coperite.png'
 import map from './map.png'
 import plus from './plus.png'
 import minus from './minus.png'
-//import green_plus from './green_plus.png'
-//import green_minus from './green_minus.png'
+import point from './point.png'
+
 
 document.querySelector(".map").style.backgroundImage = `url("${map}")`;
 document.querySelector(".plus").style.backgroundImage = `url("${plus}")`
 document.querySelector(".minus").style.backgroundImage = `url("${minus}")`
-//document.querySelector(".plus:active").style.backgroundImage =`url("${green_plus}")`
-//document.querySelector(".minus:active").style.backgroundImage =`url("${green_minus}")`
 document.getElementsByClassName('bambuk')[0].src = bambuk;
 document.getElementsByClassName('bambuk')[1].src = bambuk;
 document.querySelector('.coperite').style.backgroundImage = `url("${coperite}")`;
+
+let points = document.querySelectorAll('.point');
+
+for(let p of points){
+    p.style.backgroundImage = `url("${point}")`;
+}
